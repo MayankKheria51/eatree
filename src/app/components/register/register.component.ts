2179a6a5f100d7322b21ecf598920fdd9c2a50ef
Abhinav Kumar <Abhinav.Kumar@thomsonreuters.com>
2020-11-03 16:25:02 +0530

    registration

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -14,6 +14,8 @@ export class RegisterComponent implements OnInit {
   isSpinnerhidden: boolean = true;
   match: boolean = false;
   password = "P@$$word"
+  isRegisterhidden: boolean = true;
+  RegisterSuccessful: boolean = false;
 
   constructor() { }
 
@@ -21,17 +23,32 @@ export class RegisterComponent implements OnInit {
   }
 
   onChange(event) {
-    // this.files = event.srcElement.files;
+     this.files = event.srcElement.files;
+     console.log(this.files);
     // if(this.files){
       this.isSpinnerhidden = false;
     setTimeout(() => {
       console.log('hide');
       this.isSpinnerhidden = true;
       this.isPasswordhidden = false;
+      if(this.files[0].name.split('.')[0] === 'Aug'){
+        this.password = 'Password';
+        this.match = false;
+        this.isRegisterhidden = true;
+      } else {
+        this.password = 'P@$$word';
+        this.match = true;
+        this.isRegisterhidden = false;
+      }
     }, 2000);
     
-
+   
  
+  }
+
+  Register(){
+    this.RegisterSuccessful = true;
+    console.log(this.RegisterSuccessful);
   }
 
   focusOutFunction(){
